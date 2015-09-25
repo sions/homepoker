923f8ea70b9a4180da25dc54c34b13ac62e04714
Sion Schori <sion.schori@gmail.com>
2015-09-25 16:59:08 +0300

    Use Jinja template for HTML

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+import os
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+  template = JINJA_ENVIRONMENT.get_template('index.html')
+  return template.render({})
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+  """Return a custom 404 error."""
+  return 'Sorry, nothing at this URL.', 404
